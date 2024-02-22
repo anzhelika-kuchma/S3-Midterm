3d29ce361c4c7241e6d62d23dd0119430756ddde
Anzhelika Kuchma <anzhelikakuchma@anzhelikas-air.home>
2024-02-22 14:42:43 -0330

    added all mock data

---
+++ Database/Mock_data/categories.sql
@@ -0,0 +1,7 @@
+INSERT INTO Categories (name, description) VALUES
+('Tops', 'Includes shirts, blouses, and tops of various styles.'),
+('Bottoms', 'Includes pants, shorts, skirts, and trousers.'),
+('Dresses', 'Includes various types of dresses for different occasions.'),
+('Outerwear', 'Includes jackets, coats, and sweaters for outerwear.'),
+('Footwear', 'Includes shoes, boots, and sandals for footwear.'),
+('Accessories', 'Includes bags, hats, scarves, and belts for accessories.');
